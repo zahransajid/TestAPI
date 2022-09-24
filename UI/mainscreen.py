215b268a9fb06f8aac1d488d67f10b221c27c46a
zahransajid <zahransajid3@gmail.com>
2022-09-25 02:12:27 +0530

    initial commit

--- UI/mainscreen.py
+++ UI/mainscreen.py
@@ -1,22 +1,13 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.uix.dropdown import DropDown
-from kivy.uix.button import Button
-class MainScreen(GridLayout):
+
+class Main(GridLayout):
 
     def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
+        super(Main, self).__init__(**kwargs)
         self.cols = 2
-        dropdown = DropDown()
-        for index in ["GET","POST"]:
-            btn = Button(text='%s' % index, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-            dropdown.add_widget(btn)
-        mainbutton = Button(text='Hello', size_hint=(None, None))
-        mainbutton.bind(on_release=dropdown.open)
-        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-        self.add_widget(dropdown)
+        self.add_widget(Label(text='User Name'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
         self.add_widget(Label(text='password'))
